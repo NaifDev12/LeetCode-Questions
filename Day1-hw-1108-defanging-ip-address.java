88306065919e5ccc73c1d9e2114de27f655867ac
Shaik Mohammed Naif <naif.dhanish@gmail.com>
2026-07-17 11:41:57 +0530

    Add defangIPaddr method to defang IP address
    Implement defangIPaddr method to replace '.' with '[.]'.

---
+++ Day1-hw-1108-defanging-ip-address.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String defangIPaddr(String address) {
+
+        return address.replace(".","[.]");// uses the replace method which is inbuilt in the java compiler which is used to replace characters with required characters
+      
+        
+    }
+}
